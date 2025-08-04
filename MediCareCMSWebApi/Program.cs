cac1bfb5e90c8ad678e346811c8e49bc158c9f52
steffil13 <ashinsteffil13@gmail.com>
2025-08-04 11:04:11 +0530

    Login updated with JWT

--- MediCareCMSWebApi/Program.cs
+++ MediCareCMSWebApi/Program.cs
@@ -1,3 +1,8 @@
+using MediCareCMSWebApi.Models;
+using MediCareCMSWebApi.Repository;
+using MediCareCMSWebApi.Service;
+using Microsoft.EntityFrameworkCore;
+
 namespace MediCareCMSWebApi
 {
     public class Program
@@ -9,8 +14,39 @@ namespace MediCareCMSWebApi
             // Add services to the container.
 
             builder.Services.AddControllers();
+            //cors policy
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllOrigin", builder =>
+                {
+                    builder.WithOrigins("http://localhost:4200")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials();
+                });
+            });
+
+            builder.Services.AddDbContext<MediCareDbContext>(
+                options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+            );
+            builder.Services.AddScoped<ILoginRepository, LoginRepository>();
+            builder.Services.AddScoped<ILoginService, LoginService>();
+
+            //swagger 
+            builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
+
+            //swagger
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            //Enable CORS
+            app.UseCors("AllowAllOrigin");
 
             // Configure the HTTP request pipeline.
 
